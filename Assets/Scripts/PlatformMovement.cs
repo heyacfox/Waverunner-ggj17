20c8cdfb4423dabf81731123c305cbcf4039cdb5
heyacfox <heyacfox@gmail.com>
2017-01-20 21:48:19 -0500

    Right-Running

---
+++ Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour {
+
+	Rigidbody2D rb2d;
+
+	void Awake() {
+		rb2d = this.GetComponent<Rigidbody2D> ();
+		rb2d.velocity = new Vector2 (-1, 0);
+
+	}
+}
---
+++ Assets/Scripts/RunningCharacter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunningCharacter : MonoBehaviour {
+
+	Rigidbody2D rb2d;
+
+	void Awake() {
+		rb2d = this.GetComponent<Rigidbody2D> ();
+		rb2d.velocity = new Vector2 (0, 0);
+
+	}
+	
+
+}
