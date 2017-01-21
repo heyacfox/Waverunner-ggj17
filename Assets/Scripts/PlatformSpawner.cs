768492be73747751e683e923ee7ac2ccf209a100
heyacfox <heyacfox@gmail.com>
2017-01-20 21:56:55 -0500

    Platform Spawning

---
+++ Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour {
+
+	float timeLeft = 6;
+	public GameObject largePlatform;
+
+	// Update is called once per frame
+	void Update () {
+
+		timeLeft -= Time.deltaTime;
+		if (timeLeft < 0) {
+			
+			timeLeft = 10;
+			Instantiate (largePlatform, new Vector3 (5, -2 + Random.value), Quaternion.identity);
+		}
+	}
+}
